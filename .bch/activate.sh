7e95d335e0d5e901b39aa99cb9a87e260fa42d09
Bryan Hann <nobody@nowhere>
2025-06-11 13:08:24 -0230

    Reduce dependancy on [bch:000:*] functions.

--- .bch/activate.sh
+++ .bch/activate.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-__export() { export BCH_JOTS_${1}="${2}"; }
-__export _root $(dirname $(dirname ${BASH_SOURCE[0]}))
-__export _bin  ${BCH_JOTS__root}/.bch.bin
-unset __export
-
-bch:000:linkall ${BCH_JOTS__bin}
+export BCH_JOTS__root=$(dirname $(dirname $BASH_SOURCE))
+source $(dirname $BASH_SOURCE)/init/fn.sh
+::lbin:: $(dirname $BASH_SOURCE)/lbin
